1790033ab5a163ed8be581f94d822a0b6d02ec17
jayce <jaycethanks@outlook.com>
2022-08-11 20:05:59 +0800

    update

---
+++ vue3-demo/src/components/Article/ArticleHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2 class="font-bold text-black-700">
+      <slot name="title" :data="obj"></slot>
+    </h2>
+    <h3 class="text-sm text-black-700">
+      <slot name="subtitle"></slot>
+    </h3>
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive } from 'vue';
+const obj = {
+  site: 'www.example.com',
+  email: 'jay@123.com',
+  phone: '1312132134'
+}
+</script>
--- vue3-demo/src/components/HelloWorld.vue
+++ vue3-demo/src/components/HelloWorld.vue
@@ -1,29 +1,16 @@
 <script setup lang="ts">
-import { watch, reactive } from "vue"
-let name = reactive({
-  inner: {
-    fname: '',
-    deepinner: {
-      lname: ''
-    }
-  }
-})
-watch(name.inner, (newVal, oldVal) => {
-  console.log(newVal, oldVal)
-})
+const emit = defineEmits(['cusEmit01', 'cusEmit02'])
 
+const handleClick = () => {
+  emit('cusEmit01', [1, 2, 3, 4, 5])
 
+}
+const handleClick02 = () => {
+  emit('cusEmit02', { a: 1, b: 2, c: 3 })
+}
 </script>
 
 <template>
-  <form action="#">
-    <div>
-      <label for="fname">First Name:</label>
-      <input v-model="name.inner.fname" type="text" name="fname">
-    </div>
-    <div>
-      <label for="lname">Last Name:</label>
-      <input v-model="name.inner.deepinner.lname" type="text" name="lname">
-    </div>
-  </form>
+  <button @click="handleClick">EmitEvent Test 01</button>
+  <button @click="handleClick02">EmitEvent Test 02</button>
 </template>
