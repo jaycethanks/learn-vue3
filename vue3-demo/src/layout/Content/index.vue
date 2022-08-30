9d7484f1ac2bd24be860dae4d7aa1bc258e50b36
jayce <jaycethanks@outlook.com>
2022-08-30 22:52:49 +0800

    2022年 08月 30日 星期二 22:52:49 CST

--- vue3-demo/src/layout/Content/index.vue
+++ vue3-demo/src/layout/Content/index.vue
@@ -1,23 +1,15 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
+import { useRouter, RouterLink, RouterView } from "vue-router";
+const {
+  options: { routes },
+} = useRouter();
+console.log(routes);
 </script>
 
 <template>
   <div>
-    <nav
-      class="w-full text-xs flex gap-3 mx-5 my-3 underline underline-offset-2"
-    >
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/globalComponent">Global Component</RouterLink>
-      <RouterLink to="/article">Article</RouterLink>
-      <RouterLink to="/propandemit">Props and Emits</RouterLink>
-      <RouterLink to="/slotscope">Slot Scope</RouterLink>
-      <RouterLink to="/noneProps">None Props</RouterLink>
-      <RouterLink to="/dynamicslot">Dynamic Slot</RouterLink>
-      <RouterLink to="/asyncgallery">Async Component</RouterLink>
-      <RouterLink to="/teleport">Teleport</RouterLink>
-      <RouterLink to="/transition">Transition</RouterLink>
-      <RouterLink to="/keepalive">Keep Alive</RouterLink>
+    <nav class="w-full text-xs flex flex-wrap gap-3 mx-5 my-3 underline underline-offset-2">
+      <RouterLink v-for="(route, index) in routes" :to="route.path" :key="route.name">{{ index }}. {{ route.meta.desc }}</RouterLink>
     </nav>
     <hr />
     <RouterView></RouterView>
--- vue3-demo/src/router/index.ts
+++ vue3-demo/src/router/index.ts
@@ -8,58 +8,87 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: { desc: 'Home' }
     },
 
     {
       path: "/globalComponent",
       name: "globalComponent",
+      meta: { desc: 'Global Component' },
+
       component: () => import("../views/GlobalComps/index.vue"),
     },
     {
       path: "/article",
       name: "article",
+      meta: { desc: 'Article' },
+
       component: () => import("../views/Article/index.vue"),
     },
     {
       path: "/propandemit",
       name: "propandemit",
+      meta: { desc: 'Props and Emits' },
+
       component: () => import("../views/PropsEmits/index.vue"),
     },
     {
       path: "/slotscope",
       name: "slotscope",
+      meta: { desc: 'Slot Scope' },
+
       component: () => import("../views/SlotScope/index.vue"),
     },
     {
       path: "/noneProps",
       name: "noneProps",
+      meta: { desc: 'None Props' },
+
       component: () => import("../views/NoneProps/index.vue"),
     },
     {
       path: "/dynamicslot",
       name: "dynamicslot",
+      meta: { desc: 'Dynamic Slot' },
+
       component: () => import("../views/DynamicSlot/index.vue"),
     },
     {
       path: "/asyncgallery",
       name: "asyncgallery",
+      meta: { desc: 'Async Component' },
+
       component: () => import("../views/AsyncComponent/index.vue"),
     },
     {
       path: "/keepalive",
       name: "keepalive",
+      meta: { desc: 'Keep Alive' },
+
       component: () => import("../views/KeepAlive/index.vue"),
     },
     {
       path: "/teleport",
       name: "teleport",
+      meta: { desc: 'Teleport' },
+
       component: () => import("../views/Teleport/index.vue"),
     },
     {
       path: "/transition",
       name: "transition",
+      meta: { desc: 'Transition' },
+
       component: () => import("../views/Transition/index.vue"),
     },
+    {
+      path: "/eventNested",
+      name: "eventNested",
+      meta: { desc: 'EventNested' },
+
+      component: () => import("../views/EventNestedComps/index.vue"),
+    },
+
   ],
 });
 
