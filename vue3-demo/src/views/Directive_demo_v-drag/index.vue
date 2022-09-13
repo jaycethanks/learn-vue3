8213b685480945f0d7dcecd618e19e4c1eb8278e
jayce <jaycethanks@outlook.com>
2022-09-13 23:32:33 +0800

    2022年 09月 13日 星期二 23:32:33 CST

--- vue3-demo/src/views/Directive_demo_v-drag/index.vue
+++ vue3-demo/src/views/Directive_demo_v-drag/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import MoveBox from "./MoveBox.vue";
 import type { Directive, DirectiveBinding } from "vue";
+const timeStamp = new Date().getTime();
+const boxClassName = `box-${timeStamp}`;
+const pointerClassName = `pointer-${timeStamp}`;
 const vDrag: Directive<any, void> = (el: HTMLElement, binding: DirectiveBinding) => {
-  const { indicator } = binding.modifiers;
+  const { indicator, keep } = binding.modifiers;
   let moveElement: HTMLDivElement = el.firstElementChild as HTMLDivElement;
+  attachRelatedClass(el);
   const mouseDown = (e: MouseEvent) => {
     indicator && addPointerIndicator();
     let x = e.clientX - el.offsetLeft;
@@ -15,24 +19,63 @@ const vDrag: Directive<any, void> = (el: HTMLElement, binding: DirectiveBinding)
     };
     document.addEventListener("mousemove", move);
     document.addEventListener("mouseup", () => {
+      !keep && removePointerIndicator();
       document.removeEventListener("mousemove", move);
     });
   };
   moveElement.addEventListener("mousedown", mouseDown);
 };
 const addPointerIndicator = () => {
-  const pointer: HTMLDivElement = document.querySelector(".pointer") || document.createElement("div");
-  pointer.classList.add("box", "pointer");
+  const pointer: HTMLDivElement = document.querySelector(`.${pointerClassName}`) || document.createElement("div");
+  pointer.classList.add(boxClassName, pointerClassName);
   document.body.appendChild(pointer);
 };
 const updatePointerIndicator = (e: MouseEvent) => {
-  const pointer: HTMLDivElement = document.querySelector(".pointer") || document.createElement("div");
+  const pointer: HTMLDivElement = document.querySelector(`.${pointerClassName}`) || document.createElement("div");
   pointer.style.left = e.clientX + "px";
   pointer.style.top = e.clientY + "px";
+};
+const removePointerIndicator = () => {
+  const pointer: HTMLDivElement = document.querySelector(`.${pointerClassName}`);
+  pointer && document.body.removeChild(pointer);
+};
+const attachRelatedClass = (el: HTMLElement) => {
+  el.classList.add(boxClassName);
+  const style = document.createElement("style");
+  style.textContent = `
+.${boxClassName} {
+  position: absolute;
+}
+.${boxClassName}::before {
+  content: "";
+  width: 1px;
+  border-top: 1px dashed;
+  left: -100vw;
+  width: 200vw;
+  top: -2px;
+  position: absolute;
+}
+.${boxClassName}::after {
+  content: "";
+  width: 1px;
+  border-right: 1px dashed;
+  left: -2px;
+  height: 200vh;
+  top: -100vh;
+  position: absolute;
+}
+.${pointerClassName}::before {
+  border-top: 1px dashed red;
+}
+.${pointerClassName}::after {
+  border-right: 1px dashed red;
+}
+`;
+  document.body.appendChild(style);
 };
 </script>
 <template>
   <div class="h-full bg-gray-200">
-    <MoveBox v-drag.indicator></MoveBox>
+    <MoveBox v-drag.indicator.keep></MoveBox>
   </div>
 </template>
